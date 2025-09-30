8bf2b711d52f6a3fc4813a05fc2aa29a8a442851
doctosstream-max <doctos.stream@gmail.com>
2025-09-30 07:22:22 -0300

    Update main.py
    Signed-off-by: doctosstream-max <doctos.stream@gmail.com>

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
+from openai import OpenAI
 import os
 import discord
-import openai
 
-# Definimos los intents obligatorios para discord.py 2.x
-intents = discord.Intents.default()
-intents.message_content = True  # permite leer el contenido de los mensajes
-
-# Creamos el client con los intents
-client = discord.Client(intents=intents)
-
-# Configuramos la API key de OpenAI desde variables de entorno
-openai.api_key = os.environ['OPENAI_API_KEY']
+client = discord.Client(intents=discord.Intents.default())
+openai_client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 @client.event
 async def on_message(message):
-    # Ignorar mensajes del propio bot
     if message.author == client.user:
         return
-
-    # Revisar que sea el canal correcto
     if message.channel.name == "auri-chat":
-        respuesta = openai.ChatCompletion.create(
+        response = openai_client.chat.completions.create(
             model="gpt-4",
             messages=[{"role": "user", "content": message.content}]
         )
-        await message.channel.send(respuesta['choices'][0]['message']['content'])
+        await message.channel.send(response.choices[0].message.content)
 
-# Ejecutar el bot con el token de Discord desde variables de entorno
 client.run(os.environ['DISCORD_TOKEN'])
